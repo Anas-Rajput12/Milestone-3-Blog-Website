27f5bf3b28b2f1febcb9446b177b08f8a15bd2a8
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2025-01-05 16:36:11 +0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Image from "next/image";
 import Link from "next/link";
 import { FaHome, FaUserAlt, FaPhoneAlt, FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
